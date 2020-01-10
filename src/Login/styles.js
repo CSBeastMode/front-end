d5c4f01fe582654820eae2d3ebd8032659e677ef
Laura Daugherty <lauradaugherty6@gmail.com>
2020-01-09 18:12:13 -0800

    changing .main to .div

--- src/Login/styles.js
+++ src/Login/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-export const Main = styled.main`
+export const Main = styled.div`
     display: flex;
     justify-content: center;
     align-items: center; 
